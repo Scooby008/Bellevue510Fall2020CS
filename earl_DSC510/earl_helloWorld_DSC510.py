4db18b566a248598793744a89af927c92a1d4981
E. Earl <esanch93@calstatela.edu>
2020-09-01 14:10:26 -0700

    Added header

--- earl_DSC510/earl_helloWorld_DSC510.py
+++ earl_DSC510/earl_helloWorld_DSC510.py
@@ -1,1 +1,9 @@
+"""
+DSC 510
+Week 1
+1.2 Programming Assignment: Hello World
+Author: Elizabeth Earl
+08/31/20
+Purpose: The purpose of this program is to display a simple "Hello World" to user
+"""
 print("Hello World")
