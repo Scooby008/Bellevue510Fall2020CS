0014ef1771ff317c3458f5bdadffb61b36040685
Mike Eller <mike.eller65@gmail.com>
2020-09-01 09:44:27 -0500

    Added eller_DSC510 directory and hello world file

---
+++ eller_DSC510/ellerHelloWorld.py
@@ -0,0 +1,8 @@
+# DSC510
+# Week 1
+# Example Hello World Program
+# Author Mike Eller
+# 08/31/20
+# The purpose of this program is to demonstrate that we all know how to create a simple program to print Hello World
+
+print("Hello class and welcome to DSC510")
