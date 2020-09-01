feb60438003fd5b64d1b4cf5129774f68504115e
razrbak501 <70500291+razrbak501@users.noreply.github.com>
2020-09-01 16:52:50 -0500

    Delete parisHelloWorld.py

--- parisHelloWorld.py
+++
@@ -1,8 +0,0 @@
-# DSC510
-# Week 1
-# Assignment 1.1, Hello World
-# Author Michael Paris
-# 08/31/20
-# Purpose of this program is to create a simple program and upload it to Github
-
-print("Hello World")
