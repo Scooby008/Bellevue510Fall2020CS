8b770658307078244b2f775048dc8ada81f51bc0
razrbak501 <70500291+razrbak501@users.noreply.github.com>
2020-09-01 16:50:58 -0500

    Add files via upload

---
+++ parisHelloWorld.py
@@ -0,0 +1,8 @@
+# DSC510
+# Week 1
+# Assignment 1.1, Hello World
+# Author Michael Paris
+# 08/31/20
+# Purpose of this program is to create a simple program and upload it to Github
+
+print("Hello World")
