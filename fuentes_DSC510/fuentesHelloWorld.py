d407af97f759de2c31a6badbaebe195250e3be19
astrid <asfuentes@my365.bellevue.edu>
2020-09-01 18:29:55 -0400

    Created fuentesHelloWorld.py program

--- fuentes_DSC510/fuentesHelloWorld.py
+++ fuentes_DSC510/fuentesHelloWorld.py
@@ -3,7 +3,8 @@
 # Hello World Program
 # Author: Astrid Fuentes
 # Date: 01-Sep-2020
-# The purpose of this program is to print “Hello World”.
+# The purpose of this program is to print “Hello World”
+# and to demonstrate the correct setup of PyCharm and Python.
 
 
 print("Hello World")
