54d6591413cdfd5a191ac77e27689598ac058a12
rodhk001 <70448343+rodhk001@users.noreply.github.com>
2020-09-01 20:44:46 -0700

    Delete HelloWorld.py

--- HelloWorld.py
+++
@@ -1,8 +0,0 @@
-# DSC510
-# Week 1
-# Hello World Assignment 1
-# Author Kiana Gonzalez-Rodholm
-# 8/31/2020
-# This program is meant to simply display the phrase "Hello World" in order to get used to the program.
-
-print("Hello World!")
