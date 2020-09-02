44d47d46b0da42f1da8b48dc707fbc0cb9fb534a
Patel <pvpatel@bidmc.harvard.edu>
2020-09-02 16:37:32 -0400

    Patel - Hello World

---
+++ Patel_DSC510/HelloWorld.py
@@ -0,0 +1,8 @@
+# DSC510
+# Week 1 Assignment
+# Example Hello World Program
+# Parth Patel
+# 9/2/2020
+# The purpose of this program is to show how to create a simple program and upload it to github
+
+print("Hello World")
