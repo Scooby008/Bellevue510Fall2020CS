ba48b954e5c991c9d25e22ed48c6de0ddf0df96d
arun86t <70536647+arun86t@users.noreply.github.com>
2020-09-02 08:25:41 -0700

    Hello World

---
+++ Thangaraj_DSC510/thangarajHelloWorld.py
@@ -0,0 +1,8 @@
+# DSC510 : Introduction to programming
+# Week 1
+# Example : Hello World Program
+# Author : Arunkumar Thangaraj
+# Date : 09/02/20
+# The purpose of this program is to demonstrate that we know to create a simple program to print Hello World and upload to Github
+
+print("Hello World")
