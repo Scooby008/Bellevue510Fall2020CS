e391a66a88d9d79e43237065846a2db4d8b65fdc
rodhk001 <70448343+rodhk001@users.noreply.github.com>
2020-09-01 20:34:22 -0700

    Hello World program

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+# DSC510
+# Week 1
+# Hello World Assignment 1
+# Author Kiana Gonzalez-Rodholm
+# 8/31/2020
+# This program is meant to simply display the phrase "Hello World" in order to get used to the program.
+
+print("Hello World!")
