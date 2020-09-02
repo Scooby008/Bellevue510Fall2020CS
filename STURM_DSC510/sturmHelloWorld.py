473a1adcedd93ba5105e33b2060e2d5ec4814675
Isabella Sturm <isabellasturm@macbook-pro.home>
2020-09-01 20:34:41 -0400

    Created student directory, included hello world program

---
+++ STURM_DSC510/sturmHelloWorld.py
@@ -0,0 +1,9 @@
+# DSC510
+# Week 1
+# Hello World program
+# Author Isabella Sturm
+# 08/31/2020
+# This program is to demonstrate the basic understanding of using Python and PyCharm as well as using GitHub
+
+
+print("Hello World")
