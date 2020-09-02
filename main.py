d2ebda258beb9da3927b788cb1284661d8949d07
kbauer219 <69987557+kbauer219@users.noreply.github.com>
2020-09-02 16:45:49 -0500

    Bauer Hello World

---
+++ main.py
@@ -0,0 +1,7 @@
+# T DSC510
+# # Week 1
+# # Example Hello World Program
+# # Author Kimberly Bauer
+# # 09/02/20
+# # The purpose of this program is to demonstrate that we all know how to create a simple program and upload it to GitHub.
+print("Hello World")
