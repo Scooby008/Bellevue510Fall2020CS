0def4a75bd9cf896cdc65811d07b3a0b101709b7
Lucas9e <70613382+Lucas9e@users.noreply.github.com>
2020-09-02 20:29:59 -0400

    adding hello program

---
+++ Edmisten_DSC510/hello_world.py
@@ -0,0 +1,8 @@
+# DSC510
+# Week 1
+# Example Hello World Program
+# Author Lucas Edmisten
+# 8/31/20
+# THe purpose of the Hello World program is to show our understanding of basic programming in PyCharm
+
+print("hello_world")
