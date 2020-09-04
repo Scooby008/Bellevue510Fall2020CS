4ef98f302bb46ecf0fc8e48803014c277d5ed2d8
ceklatt <51460769+ceklatt@users.noreply.github.com>
2020-09-04 19:38:22 -0400

    Klatt_Courtney_DSC510

---
+++ Klatt_Courtney_DSC510.py
@@ -0,0 +1,12 @@
+# Courtney Klatt
+# DSC 510 Assignment 1
+# 4 September 2020
+print('Hello World')
+print('What is your name?')     # ask for the user's name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')      # ask for user's age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
