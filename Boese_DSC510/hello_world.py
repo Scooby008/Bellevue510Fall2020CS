5492e0faa72d7216d3ca10e8c363ec39ff987500
Orrin Boese <orrin.boese@live.com>
2020-09-04 16:57:26 -0600

    adding hello_world program by Orrin Boese

---
+++ Boese_DSC510/hello_world.py
@@ -0,0 +1,17 @@
+# DSC510
+# Week 1
+# Hello world! program
+# Author: Orrin Boese
+# 09/04/20
+# The timeless classic, hello world program...
+
+# define hello world function to print hello world
+def hello_world():
+    print("Hello, world!")
+
+
+# call hello world function
+hello_world()
+
+# echoes in the background
+print("*echoes*")
