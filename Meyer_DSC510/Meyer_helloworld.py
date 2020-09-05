9cdbb4522935de7c8246c3f2c4a64127f2be718e
Jake Meyer <jakmeyer@my365.bellevue.edu>
2020-09-05 08:49:54 -0600

    Added Meyer Hello World

---
+++ Meyer_DSC510/Meyer_helloworld.py
@@ -0,0 +1,10 @@
+# DSC 510 Introduction to Programming
+# Week 1
+# Hello World Program
+# Author Jake Meyer
+# 09/04/2020
+# Purpose: Print hello world for Assignment 1.1
+
+print("hello world")
+
+# End of Program
