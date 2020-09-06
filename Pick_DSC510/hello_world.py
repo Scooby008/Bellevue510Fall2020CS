285de17e52b5f27f905d2ece17011776dbcf3cac
cameronpick1 <70823751+cameronpick1@users.noreply.github.com>
2020-09-05 22:03:28 -0500

    Pick_Hello World

---
+++ Pick_DSC510/hello_world.py
@@ -0,0 +1,8 @@
+# DSC 510
+# Week 1
+# Example Hello World Program
+# Author Cameron Pick
+# 09/5/20
+# The purpose of this program is to demonstrate that we all know how to create a simple program to print Hello World
+
+print('Hello World')
