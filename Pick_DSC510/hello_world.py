1a33967524dfeeeb294b80be37e18cc49d956940
cameronpick1 <70823751+cameronpick1@users.noreply.github.com>
2020-09-05 22:02:31 -0500

    Delete hello_world.py

--- Pick_DSC510/hello_world.py
+++
@@ -1,8 +0,0 @@
-# DSC 510
-# Week 1
-# Example Hello World Program
-# Author Cameron Pick
-# 09/5/20
-# The purpose of this program is to demonstrate that we all know how to create a simple program to print Hello World
-
-print('Hello World')
