08ab3c3665a2964d2aaeb284223ad37afa0a38ee
Pkarlovic <70538240+Pkarlovic@users.noreply.github.com>
2020-09-06 15:03:21 -0700

    Add files via upload

---
+++ Karlovic_DSC510/Hello_World.py
@@ -0,0 +1,8 @@
+#DSC510
+#Week1
+#Example Hello World Program
+#Author: Patrik Karlovic
+#Date: 8/31/2020
+#Purpose: Create a hello world program to begin introduction to basics of coding.
+
+print("Hello, World!")
