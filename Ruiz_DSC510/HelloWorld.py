37fb5cd88b27f758b89629b60fcbf18d1173fa5c
JRUIZ511 <60910810+JRUIZ511@users.noreply.github.com>
2020-09-06 21:28:40 -0500

    Create HelloWorld.py
    Adding HelloWorld to master Joseph Ruiz

---
+++ Ruiz_DSC510/HelloWorld.py
@@ -0,0 +1,12 @@
+"""
+# DSC510
+# Week 1
+#  Hello World Program
+# Author: Joseph
+# 09/06/20
+# The purpose of this program is to demonstrate that we all know how to create a simple program and upload it to GitHub
+"""
+
+
+
+print("Hello World")
